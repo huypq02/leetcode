84da88057d398086b5287dc1a2befabe6d238746
Huy Pham <huypham0297@gmail.com>
2025-08-31 23:12:37 +0700

    feat: add the exercise related to dynamic sliding window and array

--- Arrays/best-time-to-buy-and-sell-stock/main.go
+++ Arrays/best-time-to-buy-and-sell-stock/main.go
@@ -1,17 +1,37 @@
 package main
 
-import (
-    "fmt"
-)
-
 func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	left, maxProfit := 0, 0
+	for right := 1; right < len(prices); right++ {
+		if prices[left] > prices[right] {
+			left = right
+			continue
+		}
+		profit := prices[right] - prices[left]
+		if maxProfit < profit {
+			maxProfit = profit
+		}
+	}
 
+	return maxProfit
 }
 
-func main(){
-// Input: prices = [7,1,5,3,6,4]
+func main() {
+	// Input: prices = [7,1,5,3,6,4]
+	prices := []int{7, 1, 5, 3, 6, 4}
+	// Output: 5
+	println(maxProfit(prices))
 
-// Output: 5
-// Input: prices = [7,6,4,3,1]
-// Output: 0
+	// Input: prices = [7,6,4,3,1]
+	prices2 := []int{7, 6, 4, 3, 1}
+	// Output: 0
+	println(maxProfit(prices2))
+
+	// Input: [2,4,1]
+	prices3 := []int{2, 4, 1}
+	// Output: 2
+	println(maxProfit(prices3))
 }
