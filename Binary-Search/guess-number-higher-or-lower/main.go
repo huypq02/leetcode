06a55fde8a493ac2f5f3660a183417809f2d6c19
Huy Pham <huypham0297@gmail.com>
2025-11-15 23:36:58 +0700

    feat: add the binary-search exercise, guess number higher or lower

---
+++ Binary-Search/guess-number-higher-or-lower/main.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+	return 0
+}
+
+func main() {
+	// Test case 1: n = 10, pick = 6
+	// Expected output: 6
+	// (Assume guess API is implemented elsewhere)
+	// fmt.Println(guessNumber(10))
+
+	// Test case 2: n = 1, pick = 1
+	// Expected output: 1
+	// fmt.Println(guessNumber(1))
+
+	// Test case 3: n = 2, pick = 1
+	// Expected output: 1
+	// fmt.Println(guessNumber(2))
+}
