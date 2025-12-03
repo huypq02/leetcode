1b9727b2f52692bb92650bcf1d01eaa8fdb5da20
Huy Pham <huypham0297@gmail.com>
2025-12-03 23:49:51 +0700

    feat: add testcases for the exercise

--- Greedy/can-place-flowers/main.go
+++ Greedy/can-place-flowers/main.go
@@ -6,5 +6,31 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
 }
 
 func main() {
-	//
+	// Example 1
+	flowerbed1 := []int{1, 0, 0, 0, 1}
+	n1 := 1
+	// Expected: true
+	println("Testcase 1:", canPlaceFlowers(flowerbed1, n1))
+
+	// Example 2
+	flowerbed2 := []int{1, 0, 0, 0, 1}
+	n2 := 2
+	// Expected: false
+	println("Testcase 2:", canPlaceFlowers(flowerbed2, n2))
+
+	// Additional testcases
+	flowerbed3 := []int{0, 0, 1, 0, 0}
+	n3 := 2
+	// Expected: true
+	println("Testcase 3:", canPlaceFlowers(flowerbed3, n3))
+
+	flowerbed4 := []int{0}
+	n4 := 1
+	// Expected: true
+	println("Testcase 4:", canPlaceFlowers(flowerbed4, n4))
+
+	flowerbed5 := []int{1}
+	n5 := 1
+	// Expected: false
+	println("Testcase 5:", canPlaceFlowers(flowerbed5, n5))
 }
